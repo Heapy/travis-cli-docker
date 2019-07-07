06842313dd1f82fdb377571cd534a9c373569dbb
Ruslan Ibragimov <ruslan@ibragimov.by>
2019-07-07 16:42:24 +0300

    Skipping build on master.

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+if [[ -v TRAVIS_TAG ]]; then
+  docker build --build-arg "TRAVIS_VERSION=${TRAVIS_TAG}" -t "heapy/travis-cli-docker:${TRAVIS_TAG}" -t "heapy/travis-cli-docker:latest" ./
+else
+  echo "Skipping Docker Build"
+fi
